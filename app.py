74f8a64f6806415adda377816443e198daa39e79
dominikoso <root@dominikoso.me>
2025-02-08 14:59:54 +0100

    app: Change directory creation strategy, add support for multiple models

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import csv
-import ollama
 import os
-from typing import Tuple
 from datetime import datetime
+from typing import Tuple
+
+import ollama
+
 
 def load_data(csv_path: str):
     data = []
     with open(csv_path, "r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            data.append({
-                "text": row["Text"],     
-                "label": row["Label"]    
-            })
+            data.append({"text": row["Text"], "label": row["Label"]})
     return data
 
-def analyze_sentence(sentence: str, model_name: str = "deepseek-r1:14b") -> Tuple[str, str]:
+
+def analyze_sentence(
+    sentence: str, filedir: str, model_name: str = "deepseek-r1:14b"
+) -> Tuple[str, str]:
     client = ollama.Client()
     prompt = f"""
 You are a text classification tool tasked with determining if a sentence uses manipulative language. Manipulative language includes tactics like emotional appeals, guilt-tripping, undue flattery, or any covert persuasion aimed at influencing behavior or opinions.
@@ -26,25 +28,26 @@ Classify the given sentence using ONLY ONE WORD, do not add any special characte
 
 Sentence: "{sentence}"
 """
+    d = datetime.now()
     result = client.generate(model=model_name, prompt=prompt)
-    d=datetime.now()
-    os.makedirs(f"results/test-{d.strftime('%Y_%m_%d_%I_%M')}", exist_ok=True)
-    filename = os.path.join(f"results/test-{d.strftime('%Y_%m_%d_%I_%M')}", 
-                            f"{model_name}_{d.strftime('%Y_%m_%d-%I_%M_%S_%p')}_result.txt")
-    open(filename, "w").write(prompt+"\n"+result["response"])
-    return (result['response'].splitlines()[-1], filename)
+    filename = os.path.join(
+        filedir, f"{model_name}_{d.strftime('%Y_%m_%d-%I_%M_%S_%p')}_result.txt"
+    )
+    open(filename, "w").write(prompt + "\n" + result["response"])
+    return (result["response"].splitlines()[-1], filename)
 
-def main():
-    dataset = load_data("test_data.csv")
-    model_name="deepseek-r1:14b"
+
+def perform_test_per_model(dataset: list, model_name: str, filedir: str):
     correct = 0
     isCorrect = False
 
     for i, item in enumerate(dataset, start=1):
         text = item["text"]
         true_label = item["label"]
-        print(f"[*] Analyzing text \"{text}\" with true label: {true_label}")
-        model_output, filename = analyze_sentence(text, model_name)
+        print(
+            f'[*] [{model_name}] Analyzing text "{text}" with true label: {true_label}'
+        )
+        model_output, filename = analyze_sentence(text, filedir, model_name)
         if model_output == true_label:
             isCorrect = True
             correct += 1
@@ -62,6 +65,20 @@ def main():
     total = len(dataset)
     accuracy = correct / total if total > 0 else 0
     print(f"[*] Accuracy for model {model_name}: {accuracy:.2f}")
+
+
+def main():
+    dataset = load_data("test_data.csv")
+    models = [
+        "deepseek-r1:14b",
+        "deepseek-r1:32b",
+    ]
+    d = datetime.now()
+    filedir = f"results/test-{d.strftime('%Y_%m_%d_%I_%M')}"
+    os.makedirs(filedir, exist_ok=True)
+    for model_name in models:
+        perform_test_per_model(dataset, model_name, filedir)
+
 
 if __name__ == "__main__":
     main()
